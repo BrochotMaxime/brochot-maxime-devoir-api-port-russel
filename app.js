bc0e28e3c9e5c385377267268b00e64ed965f4fc
BrochotMaxime <brochot.max@gmail.com>
2026-04-10 17:54:40 +0200

    configure express, ejs and basic css

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+const { title } = require('process');
 
 const app = express();
 
+/* Configuration du moteur de modèles EJS */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+/* Middlewares */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* Route de test */
 app.get('/', (req, res) => {
-  res.send('Port Russell API en cours de construction');
+  res.render('home', {
+        title: 'Bienvenue sur l\'API de Port Russel'
+  });
 });
 
 module.exports = app;
