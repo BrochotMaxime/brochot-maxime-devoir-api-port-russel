94231f97728ce75e6ad5aa311ce91f5a3e70fb04
BrochotMaxime <brochot.max@gmail.com>
2026-04-15 22:11:51 +0200

    create validation for users

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,32 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 const { isAuthenticated } = require('../middlewares/authMiddleware');
+const { handleValidationErrors } = require('../middlewares/validationMiddleware');
+const { createUserValidation, updateUserValidation, emailParamValidation } = require('../middlewares/userValidation');
 
-router.get('/', isAuthenticated, userController.getAllUsers);
-router.get('/:email', isAuthenticated, userController.getUserByEmail);
-router.post('/', isAuthenticated, userController.createUser);
-router.put('/:email', isAuthenticated, userController.updateUser);
-router.delete('/:email', isAuthenticated, userController.deleteUser);
+router.get('/', 
+        isAuthenticated,
+        userController.getAllUsers
+);
+
+router.get('/:email', 
+        isAuthenticated, emailParamValidation, handleValidationErrors, 
+        userController.getUserByEmail
+);
+
+router.post('/', 
+        isAuthenticated, createUserValidation, handleValidationErrors, 
+        userController.createUser
+);
+
+router.put('/:email', 
+        isAuthenticated, emailParamValidation, updateUserValidation, handleValidationErrors, 
+        userController.updateUser
+);
+
+router.delete('/:email', 
+        isAuthenticated, emailParamValidation, handleValidationErrors, 
+        userController.deleteUser
+);
 
 module.exports = router;
